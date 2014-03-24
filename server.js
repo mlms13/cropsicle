b5e388c0b188584bf52741c113d1a7954624b0bd
Jason V <jcvalle@mail.usf.edu>
2014-03-24 17:33:29 -0400

    scaffold!

---
+++ server.js
@@ -0,0 +1,30 @@
+// import the party
+var express = require('express'),
+    app = express(),
+    path = require('path');
+
+// set up middleware for all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.compress());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+// add static routes before we use router for public files
+app.use(express.static(path.join(__dirname, 'public')));
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+console.log('The NODE_ENV is ' + app.get('env'));
+
+app.use(app.router);
+
+// routes
+app.get('/', require('./app/routes/index'));
+
+
+// start that server
+app.listen(app.get('port'));;
