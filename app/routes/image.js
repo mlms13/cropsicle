dca8def3378ffeed3bd8987d21a9154f6332f446
Jason <jcvalle@mail.usf.edu>
2014-03-24 23:00:03 -0500

    fun with the settings
    can you say screen size option feature?

--- app/routes/image.js
+++ app/routes/image.js
@@ -4,7 +4,19 @@ module.exports = function (req, res) {
     // TODO: validate that we're receiving an actual URL
     // before we try to take a screenshot
 
-    webshot(req.params.url, function (err, stream) {
+    // just playing around with the options
+    var wsOptions = {
+        screenSize: {
+            width: 320,
+            height: 'all'
+        },
+        shotSize: {
+            width: 'window',
+            height: 'all'
+        }
+    };
+
+    webshot(req.params.url, wsOptions, function (err, stream) {
         // TODO: some error handling in here would also be a good thing
         // is it seriously this easy?
         stream.pipe(res);
