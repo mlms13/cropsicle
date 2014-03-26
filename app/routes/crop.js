294aaa1853105fa54e4e02edb71dc35efb5cb824
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-03-26 15:53:13 -0400

    Make sure our URLs are usable

--- app/routes/crop.js
+++ app/routes/crop.js
@@ -1,10 +1,14 @@
+function cleanUrl(url) {
+    url = url.replace('http://', '').replace('https://', '');
+    return encodeURIComponent(url);
+}
 
 module.exports.submitUrl = function (req, res) {
     // since the form should be handled with client-side Javascript,
     // if they POST to this route, we can assume Javascript is off...
 
     if (req.body.url) {
-        res.redirect('/crop/' + req.body.url)
+        res.redirect('/crop/' + cleanUrl(req.body.url));
     } else {
         // TODO: show them a message telling them they need to enter a better URL
         res.redirect('/');
@@ -15,5 +19,5 @@ module.exports.handleGetUrl = function (req, res) {
     // this route exists so that people can bookmark the link or
     // share a URL and come back to it later.
 
-    res.render('crop', { url: req.params.url});
+    res.render('crop', { url: cleanUrl(req.params.url)});
 };
